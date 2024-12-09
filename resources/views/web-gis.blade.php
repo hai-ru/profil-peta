f7832564f6ef7e7e39d5ede5328ffb783fceb661
hai-ru <mheru@student.untan.ac.id>
2024-12-09 20:20:20 +0700

    up

--- resources/views/web-gis.blade.php
+++ resources/views/web-gis.blade.php
@@ -290,6 +290,7 @@
             success:function(response){
                 if(!response.status) return alert(response.message);
                 response.data.forEach( (elm,index) => {
+                    if(geojson == null) continue;
                     for(k in elm.geojson.features){
                         if(elm.geojson.features[k].properties === undefined) continue;
                         elm.geojson.features[k].properties.style = elm.marker
